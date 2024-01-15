5d5cec7ad12b58df4917c2acee189548597cc46c
YUERONG HUANG <rowanne.h@hotmail.com>
2024-01-15 23:00:33 +1100

    shorten the code

--- index.js
+++ index.js
@@ -12,11 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials.map(sentence => {
-    return sentence.split(' ').map(word => {
-      return word[0].toUpperCase() + word.slice(1)
-    }).join(' ')
-  })
+  return tutorials.map(sentence => 
+    sentence
+    .split(' ')
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join(' ')
+  )
 }
+
 
 
